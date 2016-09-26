a8b00d4aba53a82eb044234b9cc4ab49aadc4e7a
Robert Williams <rjwil35@verizon.net>
2016-09-26 14:44:36 -0400

    initial project

---
+++ src/main/java/com/bobWilliams/Camel_CleanCode_Chp2/App.java
@@ -0,0 +1,13 @@
+package com.bobWilliams.Camel_CleanCode_Chp2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
